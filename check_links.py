8c8a2e28262d196102b29093be62f5a869361b52
Ben88881931 <310209831@qq.com>
2026-08-05 12:09:26 +0800

    Revert "更新第217期数据"
    This reverts commit ce23e4444866fc7ebc7692f6f69bb39b359d88af.

--- check_links.py
+++
@@ -1,7 +0,0 @@
-import os, re
-for f in ['尾数分析器.html', '开奖历史记录.html', '遗漏监控.html', '遗漏颜色表.html']:
-    with open(f, 'r', encoding='utf-8') as fp:
-        c = fp.read()
-    links = re.findall(r'<a href="([^"]+)">([^<]+)</a>', c)
-    nav_links = [l for l in links if '.html' in l[0]]
-    print(f'{f}: {nav_links}')
